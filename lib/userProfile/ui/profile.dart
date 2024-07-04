2c4f0d3533e7d73962e74645fad72d1dc3fed2a5
Subh211 <dassubhadeep211@gmail.com>
2024-07-04 19:51:46 +0530

    profile back problem solved

--- lib/userProfile/ui/profile.dart
+++ lib/userProfile/ui/profile.dart
@@ -24,202 +24,213 @@ class OwnProfile extends StatefulWidget {
 
 
 class _OwnProfileState extends State<OwnProfile> {
+  Future<bool> _onWillPop() async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Feed()),
+    );
+    return false;
+  }
+  
   int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetUserProfileBloc, GetUserProfileState>(
-      builder: (context, state) {
-        if (state is UserProfileInitial) {
-          BlocProvider.of<GetUserProfileBloc>(context).add(FetchUserProfileEvent());
-          return Center(child: CircularProgressIndicator());
-        } else if (state is UserProfileLoading) {
-          return Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (state is UserProfileLoaded) {
-          var profile = state.profile;
-          var user = profile.user;
-          var posts = profile.posts.posts;
+    return WillPopScope(
+        onWillPop: _onWillPop,
+        child: BlocBuilder<GetUserProfileBloc, GetUserProfileState>(
+          builder: (context, state) {
+            if (state is UserProfileInitial) {
+              BlocProvider.of<GetUserProfileBloc>(context).add(FetchUserProfileEvent());
+              return Center(child: CircularProgressIndicator());
+            } else if (state is UserProfileLoading) {
+              return Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            } else if (state is UserProfileLoaded) {
+              var profile = state.profile;
+              var user = profile.user;
+              var posts = profile.posts.posts;
 
-          double screenHeight = MediaQuery.of(context).size.height;
-          double screenWidth = MediaQuery.of(context).size.width;
-          double iconSize = screenHeight * 0.03;
+              double screenHeight = MediaQuery.of(context).size.height;
+              double screenWidth = MediaQuery.of(context).size.width;
+              double iconSize = screenHeight * 0.03;
 
 
-          return Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
-                  // Navigate back to the Feed screen with a 'refresh' result
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => Feed())
-                  );
-                },
-              ),
-              title: Text(
-                'Weave',
-                style: GoogleFonts.clickerScript(
-                  fontWeight: FontWeight.w600,
-                  color: Color.fromRGBO(165, 179, 158, 1),
-                  fontSize: AppBar().preferredSize.height,
+              return Scaffold(
+                appBar: AppBar(
+                  leading: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      // Navigate back to the Feed screen with a 'refresh' result
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Feed())
+                      );
+                    },
+                  ),
+                  title: Text(
+                    'Weave',
+                    style: GoogleFonts.clickerScript(
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromRGBO(165, 179, 158, 1),
+                      fontSize: AppBar().preferredSize.height,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                body: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        height: screenHeight * 0.16,
-                        width: screenWidth * 0.25,
-                        decoration: BoxDecoration(
-                          color: Colors.greenAccent.withOpacity(0.2),
-                          shape: BoxShape.circle,
-                        ),
-                        padding: EdgeInsets.all(12.0),
-                        child: CircleAvatar(
-                          radius: 50,
-                          backgroundImage: NetworkImage(user.photoURL!),
-                        ),
-                      ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Text(
-                            '${posts!.length}  \n Posts ',
-                            textAlign: TextAlign.center,
+                          Container(
+                            height: screenHeight * 0.16,
+                            width: screenWidth * 0.25,
+                            decoration: BoxDecoration(
+                              color: Colors.greenAccent.withOpacity(0.2),
+                              shape: BoxShape.circle,
+                            ),
+                            padding: EdgeInsets.all(12.0),
+                            child: CircleAvatar(
+                              radius: 50,
+                              backgroundImage: NetworkImage(user.photoURL!),
+                            ),
                           ),
-                          SizedBox(
-                            width: screenWidth * 0.05,
-                          ),
-                          Text(
-                            '${profile.friendDetails!.following!.length}  \n followings ',
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(
-                            width: screenWidth * 0.05,
-                          ),
-                          Text(
-                            '${profile.friendDetails!.followers!.length}  \n followers ',
-                            textAlign: TextAlign.center,
+                          Row(
+                            children: [
+                              Text(
+                                '${posts!.length}  \n Posts ',
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.05,
+                              ),
+                              Text(
+                                '${profile.friendDetails!.following!.length}  \n followings ',
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.05,
+                              ),
+                              Text(
+                                '${profile.friendDetails!.followers!.length}  \n followers ',
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
                           ),
                         ],
                       ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.045),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(user.displayName),
+                            Text(user.name),
+                            Text(user.bio!),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      Center(
+                        child: CustomTextButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => UpdateUser())
+                            );
+                          },
+                          showPencilIcon: true,
+                          showBorder: false,
+                          horizontalPadding: screenWidth * 0.26,
+                          buttonText: 'Edit Profile',
+                          screenHeight: screenHeight,
+                          buttonHeight: screenHeight * 0.06,
+                          fontSize: screenHeight * 0.022,
+                        ),
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.045),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // IconButton(
+                            //   onPressed: () {
+                            //     setState(() {
+                            //       selectedIndex = 0;
+                            //     });
+                            //   },
+                            //   icon: FaIcon(
+                            //     FontAwesomeIcons.tableCells,
+                            //     size: iconSize,
+                            //     color: selectedIndex == 0 ? Colors.black : Colors.grey,
+                            //   ),
+                            // ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  selectedIndex = 0;
+                                });
+                              },
+                              icon: FaIcon(
+                                FontAwesomeIcons.squarePlus,
+                                size: iconSize,
+                                color: selectedIndex == 0 ? Colors.black : Colors.grey,
+                              ),
+                            ),
+                            // IconButton(
+                            //   onPressed: () {
+                            //     setState(() {
+                            //       selectedIndex = 2;
+                            //     });
+                            //   },
+                            //   icon: FaIcon(
+                            //     FontAwesomeIcons.bookmark,
+                            //     size: iconSize,
+                            //     color: selectedIndex == 2 ? Colors.black : Colors.grey,
+                            //   ),
+                            // ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 3,
+                          mainAxisSpacing: 3,
+                        ),
+                        itemBuilder: (context, index) {
+                          var post = posts[index];
+                          return InkWell(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(post.image),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: posts.length,
+                      ),
                     ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.045),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(user.displayName),
-                        Text(user.name),
-                        Text(user.bio!),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: screenHeight * 0.02),
-                  Center(
-                    child: CustomTextButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => UpdateUser())
-                        );
-                      },
-                      showPencilIcon: true,
-                      showBorder: false,
-                      horizontalPadding: screenWidth * 0.26,
-                      buttonText: 'Edit Profile',
-                      screenHeight: screenHeight,
-                      buttonHeight: screenHeight * 0.06,
-                      fontSize: screenHeight * 0.022,
-                    ),
-                  ),
-                  SizedBox(height: screenHeight * 0.02),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.045),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // IconButton(
-                        //   onPressed: () {
-                        //     setState(() {
-                        //       selectedIndex = 0;
-                        //     });
-                        //   },
-                        //   icon: FaIcon(
-                        //     FontAwesomeIcons.tableCells,
-                        //     size: iconSize,
-                        //     color: selectedIndex == 0 ? Colors.black : Colors.grey,
-                        //   ),
-                        // ),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              selectedIndex = 0;
-                            });
-                          },
-                          icon: FaIcon(
-                            FontAwesomeIcons.squarePlus,
-                            size: iconSize,
-                            color: selectedIndex == 0 ? Colors.black : Colors.grey,
-                          ),
-                        ),
-                        // IconButton(
-                        //   onPressed: () {
-                        //     setState(() {
-                        //       selectedIndex = 2;
-                        //     });
-                        //   },
-                        //   icon: FaIcon(
-                        //     FontAwesomeIcons.bookmark,
-                        //     size: iconSize,
-                        //     color: selectedIndex == 2 ? Colors.black : Colors.grey,
-                        //   ),
-                        // ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: screenHeight * 0.02),
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 3,
-                      mainAxisSpacing: 3,
-                    ),
-                    itemBuilder: (context, index) {
-                      var post = posts[index];
-                      return InkWell(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(post.image),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: posts.length,
-                  ),
-                ],
-              ),
-            ),
-          );
+                ),
+              );
 
-        } else if (state is UserProfileError) {
-          return Center(child: Text('Error loading profile: ${state.error}'));
-        } else {
-          return Center(child: Text('Unexpected state'));
-        }
-      },
+            } else if (state is UserProfileError) {
+              return Center(child: Text('Error loading profile: ${state.error}'));
+            } else {
+              return Center(child: Text('Unexpected state'));
+            }
+          },
+        ),
     );
   }
 }
